598b338cbe1c2b8ec7cc03536a15821856c1d6f5
Rex Arnott <rexarnott@hotmail.com>
2020-11-12 12:17:50 +1100

    Initial commit adding code.js, index.html, stylesheet.css and words_dictionary.json

---
+++ code.js
@@ -0,0 +1,20 @@
+
+const data = require("./words_dictionary.json");
+
+
+console.log(Object.values(data))
+
+// for (let i=0;i<famousQuotes.length;i++) {
+//     let new_arr = []
+
+// }
+
+
+// Random word selector
+// function retrieveQuote() {
+//     let word = famousQuotes[Math.floor((Math.random()) * 370100)];
+//     console.log(word)
+//     getSyntaxCategory(word)
+// }
+
+
